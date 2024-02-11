7f1d67a2b44bedf1785dde9e61d990983ea85d06
Brandon Minnick <13558917+brminnick@users.noreply.github.com>
2024-02-12 10:48:45 +1100

    Update to net8.0

--- LambdaTriggers.Backend.Common/AssemblyInfo.cs
+++ LambdaTriggers.Backend.Common/AssemblyInfo.cs
@@ -1,1 +1,17 @@
-﻿[assembly: Amazon.Lambda.Core.LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
+﻿using System.Text.Json.Serialization;
+using Amazon.Lambda.APIGatewayEvents;
+using Amazon.Lambda.S3Events;
+
+namespace LambdaTriggers.Backend.Common;
+
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyRequest))]
+[JsonSerializable(typeof(APIGatewayHttpApiV2ProxyResponse))]
+[JsonSerializable(typeof(List<string>))]
+[JsonSerializable(typeof(Dictionary<string, string>))]
+[JsonSerializable(typeof(S3Event))]
+[JsonSerializable(typeof(S3Service))]
+[JsonSerializable(typeof(string))]
+[JsonSerializable(typeof(Uri))]
+public partial class CustomJsonSerializerContext : JsonSerializerContext
+{
+}
---
+++ LambdaTriggers.GetThumbnail/AssemblyInfo.cs
@@ -0,0 +1,7 @@
+﻿using Amazon.Lambda.Annotations;
+using Amazon.Lambda.Core;
+using Amazon.Lambda.Serialization.SystemTextJson;
+using LambdaTriggers.Backend.Common;
+
+[assembly: LambdaGlobalProperties(GenerateMain = true)]
+[assembly: LambdaSerializer(typeof(SourceGeneratorLambdaJsonSerializer<CustomJsonSerializerContext>))]
